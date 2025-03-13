a7158fc65d2b3fe7ba054c870659a86a8a2ad574
HinanawiTenshi <113288458+HinanawiTenshi104@users.noreply.github.com>
2025-03-13 23:25:58 +0800

    main files

--- pi.c
+++ pi.c
@@ -11,26 +11,26 @@ v=0;v<1;v+=Sv){float x=-L/2+v*L,y=R*cos(u)+Cy,z1=R*sin(u),nx=0,ny=cos(u),nz=sin(
 nyp,nzp;E(x,y,z1,A,B,&xp,&yp,&zp);E(nx,ny,nz,A,B,&nxp,&nyp,&nzp);float iz=1/(zp+K2);int px=W/2+K1*
 xp*iz,py=H/2-K1*yp*iz,l=D(nxp,nyp,nzp)*(strlen(S)-1);if(px>=0&&px<W&&py>=0&&py<H&&zp+K2<z[py*W+px])
 {z[py*W+px]=zp+K2;o[py*W+px]=S[l];}}}for(float u=0;u<6.28;u+=Su){for(float v=0;v<1;v+=Sv){float x=
-			  Lr*cos(u)-Lg,y=Cy-v*Ll,z1=			Lr*sin(u),nx=cos(u),ny=0,
-			  nz=sin(u),xp,yp,zp,nxp,nyp			,nzp;E(x,y,z1,A,B,&xp,&yp
-			  ,&zp);E(nx,ny,nz,A,B,&nxp,			&nyp,&nzp);float iz=1/(zp
-			  +K2);int px=W/2+K1*xp*iz,				py=H/2-K1*yp*iz,l=D(nxp,
-			  nyp,nzp)*(strlen(S)-1);if				(px>=0&&px<W&&py>=0&&py<H
-			  &&zp+K2<z[py*W+px]){z[py*				W+px]=zp+K2;o[py*W+px]=S[
-			  l];}}}for(float u=0;u<6.28			;u+=Su){for(float v=0;v<1;
-			  v+=Sv){float x=Lr*cos(u)+				Lg,y=Cy-v*Rl,z1=Lr*sin(u),
-			  nx=cos(u),ny=0,nz=sin(u),				xp,yp,zp,nxp,nyp,nzp;E(x,y,
-			  z1,A,B,&xp,&yp,&zp);E(nx,				ny,nz,A,B,&nxp,&nyp,&nzp);
-			  float iz=1/(zp+K2);int px				=W/2+K1*xp*iz,py=H/2-K1*yp
-			  *iz,l=D(nxp,nyp,nzp)*(/**/			strlen(S)-1);if(px>=0&&px<
-			  W&&py>=0&&py<H&&zp+K2<z[py			 *W+px]){z[py*W+px]=zp+K2;o[
-			  py*W+px]=S[l];}}}float Yb=			  Cy-Rl;for(float a=0;a<1.57;
-			  a+=Sa){float Xc=Lg+Ar*(1-				  cos(a)),Yc=Yb-Ar*sin(a);for
-			  (float v=0;v<6.28;v+=Sv2)				   {float nx=cos(v),nz=sin(v),x=
-			  Xc+Lr*nx,y=Yc,z1=Lr*nz,xp,				 yp,zp,nxp,nyp,nzp;E(x,y,z1,A,B
-			  ,&xp,&yp,&zp);E(nx,0,nz,A,				   B,&nxp,&nyp,&nzp);float iz=1/(
-			  zp+K2);int px=W/2+K1*xp*iz					 ,py=H/2-K1*yp*iz,l=D(nxp,nyp,nzp
-			  )*(strlen(S)-1);if(px>=0&&					   px<W&&py>=0&&py<H&&zp+K2<z[py*W+
-			  px]){z[py*W+px]=zp+K2;o[py						 *W+px]=S[l];}}}system("cls");for
-			  (int j=0;j<H;j++){for(int 						     i=0;i<W;i++)putchar(o[j*W+i]);
-			  putchar('\n');}A+=0.1;B+=								   0.002;}}/*Made by PH 20250313*/
+              Lr*cos(u)-Lg,y=Cy-v*Ll,z1=            Lr*sin(u),nx=cos(u),ny=0,
+              nz=sin(u),xp,yp,zp,nxp,nyp            ,nzp;E(x,y,z1,A,B,&xp,&yp
+              ,&zp);E(nx,ny,nz,A,B,&nxp,            &nyp,&nzp);float iz=1/(zp
+              +K2);int px=W/2+K1*xp*iz,             py=H/2-K1*yp*iz,l=D(nxp,
+              nyp,nzp)*(strlen(S)-1);if             (px>=0&&px<W&&py>=0&&py<H
+              &&zp+K2<z[py*W+px]){z[py*             W+px]=zp+K2;o[py*W+px]=S[
+              l];}}}for(float u=0;u<6.28            ;u+=Su){for(float v=0;v<1;
+              v+=Sv){float x=Lr*cos(u)+             Lg,y=Cy-v*Rl,z1=Lr*sin(u),
+              nx=cos(u),ny=0,nz=sin(u),             xp,yp,zp,nxp,nyp,nzp;E(x,y,
+              z1,A,B,&xp,&yp,&zp);E(nx,             ny,nz,A,B,&nxp,&nyp,&nzp);
+              float iz=1/(zp+K2);int px             =W/2+K1*xp*iz,py=H/2-K1*yp
+              *iz,l=D(nxp,nyp,nzp)*(/**/             strlen(S)-1);if(px>=0&&px<
+              W&&py>=0&&py<H&&zp+K2<z[py             *W+px]){z[py*W+px]=zp+K2;o[
+              py*W+px]=S[l];}}}float Yb=              Cy-Rl;for(float a=0;a<1.57;
+              a+=Sa){float Xc=Lg+Ar*(1-                 cos(a)),Yc=Yb-Ar*sin(a);for
+              (float v=0;v<6.28;v+=Sv2)                  {float nx=cos(v),nz=sin(v),x=
+              Xc+Lr*nx,y=Yc,z1=Lr*nz,xp,                   yp,zp,nxp,nyp,nzp;E(x,y,z1,A,B
+              ,&xp,&yp,&zp);E(nx,0,nz,A,                    B,&nxp,&nyp,&nzp);float iz=1/(
+              zp+K2);int px=W/2+K1*xp*iz                      ,py=H/2-K1*yp*iz,l=D(nxp,nyp,nzp
+              )*(strlen(S)-1);if(px>=0&&                        px<W&&py>=0&&py<H&&zp+K2<z[py*W+
+              px]){z[py*W+px]=zp+K2;o[py                          *W+px]=S[l];}}}system("cls");for
+              (int j=0;j<H;j++){for(int                               i=0;i<W;i++)putchar(o[j*W+i]);
+              putchar('\n');}A+=0.1;B+=                                    0.002;}}/*Made by PH 20250314*/
